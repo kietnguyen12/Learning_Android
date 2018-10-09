922a4acad1736ab2315577a89f879f7191e4eb22
Kiet Nguyen <roynguyen147@gmail.com>
2018-10-09 21:19:07 +0700

    add intent implicit proj

---
+++ Intent_Implicit/app/src/main/java/com/example/kietnguyen/intent_implicit/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.kietnguyen.intent_implicit;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    Button btnClick;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnClick = findViewById(R.id.btnClick);
+        btnClick.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse("https://google.com"));
+
+                startActivity(intent);
+
+            }
+        });
+    }
+}

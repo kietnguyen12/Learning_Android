12d588afc704ac9b6fad2bb4a5508f72ae2b8ef3
Kiet Nguyen <roynguyen147@gmail.com>
2018-10-10 14:27:50 +0700

    add IntentCamera proj

---
+++ IntentCamera/app/src/main/java/com/example/kietnguyen/intentcamera/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.kietnguyen.intentcamera;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.provider.MediaStore;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+    Button btnCapture;
+    ImageView imgviewHinh;
+    int REQUEST_CODE = 123;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnCapture = findViewById(R.id.btnCapture);
+        imgviewHinh = findViewById(R.id.imgviewHinh);
+
+        btnCapture.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+                startActivityForResult(intent, REQUEST_CODE);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if(requestCode == REQUEST_CODE && resultCode == RESULT_OK && data != null){
+            Bitmap bitmap = (Bitmap) data.getExtras().get("data"); // key = data la mac dinh
+            imgviewHinh.setImageBitmap(bitmap);
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+}

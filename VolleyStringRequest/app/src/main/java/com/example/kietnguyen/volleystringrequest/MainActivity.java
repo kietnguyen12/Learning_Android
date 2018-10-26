a116e04c26009d67e40482a70285bd398591e585
Kiet Nguyen <roynguyen147@gmail.com>
2018-10-26 22:42:25 +0700

    add volley string request proj

---
+++ VolleyStringRequest/app/src/main/java/com/example/kietnguyen/volleystringrequest/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.kietnguyen.volleystringrequest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        String url = "http://online.khoapham.vn/";
+
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        Toast.makeText(MainActivity.this, response, Toast.LENGTH_SHORT).show();
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(MainActivity.this, "Loi", Toast.LENGTH_SHORT).show();
+                        Log.d("AAAA", error.toString());
+                    }
+                }
+        );
+
+        requestQueue.add(stringRequest);
+    }
+}

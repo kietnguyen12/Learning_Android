65b2f8e9e7e7addf82832a6d68cf901a36fd7d0f
Kiet Nguyen <roynguyen147@gmail.com>
2018-10-28 10:25:44 +0700

    add Volley Object Request proj

---
+++ VolleyObjectRequest/app/src/main/java/com/example/kietnguyen/volleyobjectrequest/MainActivity.java
@@ -0,0 +1,51 @@
+package com.example.kietnguyen.volleyobjectrequest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        String url = "https://khoapham.vn/KhoaPhamTraining/json/tien/demo1.json";
+
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, url, null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            String monhoc = response.getString("monhoc");
+                            Toast.makeText(MainActivity.this, monhoc, Toast.LENGTH_SHORT).show();
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(MainActivity.this, "Error", Toast.LENGTH_LONG).show();
+                    }
+                }
+        );
+
+        requestQueue.add(jsonObjectRequest);
+
+    }
+}

80e1c0b5df19b46cbcfb5638a979fa9d3d33a497
Kiet Nguyen <roynguyen147@gmail.com>
2018-10-28 11:09:45 +0700

    add volley JSON ArrayRequest proj

---
+++ VolleyJSONArrayRequest/app/src/main/java/com/example/kietnguyen/volleyjsonarrayrequest/MainActivity.java
@@ -0,0 +1,46 @@
+package com.example.kietnguyen.volleyjsonarrayrequest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+
+        String url = "https://khoapham.vn/KhoaPhamTraining/json/tien/demo4.json";
+
+        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(Request.Method.GET, url, null,
+                new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        Toast.makeText(MainActivity.this, response.toString(), Toast.LENGTH_LONG).show();
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(MainActivity.this, "Error", Toast.LENGTH_LONG).show();
+                        Log.d("AAAA", error.toString());
+                    }
+                }
+        );
+
+        requestQueue.add(jsonArrayRequest);
+    }
+}
